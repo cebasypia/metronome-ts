e1c0f0e515c8202751a6c1a3efd2f18ecb88ea59
cebasypia <cebasypia.tskm8@gmail.com>
2020-08-21 15:41:19 +0900

    Add play() & stop()

--- src/components/playback.ts
+++ src/components/playback.ts
@@ -1,23 +1,37 @@
+import { Metronome } from "./metronome";
+
 // Constant definition
-const PLAY_ICON = '▶'
-const STOP_ICON = '■'
+const PLAY_ICON = "▶";
+const STOP_ICON = "■";
+
+let metronome;
+
+const play = () => {
+  metronome = new Metronome();
+  metronome.start();
+};
+
+const stop = () => {
+  metronome.stop();
+};
 
 // Get elements
-const playbackElement = document.getElementById('playback')
+const playbackElement = document.getElementById("playback");
 
 // Initialize elements
-playbackElement.innerText = PLAY_ICON
+playbackElement.innerText = PLAY_ICON;
 
 const switchPlayback = (isPlaying: boolean): void => {
-  playbackElement.innerText = isPlaying ? STOP_ICON : PLAY_ICON
-}
+  playbackElement.innerText = isPlaying ? PLAY_ICON : STOP_ICON;
+};
 
 const isPlaying = (): boolean => {
-  return playbackElement.innerHTML === PLAY_ICON
-}
+  return playbackElement.innerHTML === STOP_ICON;
+};
 
 export const addPlaybackEvent = () => {
-  playbackElement.addEventListener('click', () => {
-    switchPlayback(isPlaying())
-  })
-}
+  playbackElement.addEventListener("click", () => {
+    switchPlayback(isPlaying());
+    isPlaying() ? play() : stop();
+  });
+};
